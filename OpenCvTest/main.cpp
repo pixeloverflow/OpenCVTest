a787a8221182178f6188655d1a7a95be48737238
xiaojiakai <xiaojiakai@testbird.com>
2017-02-15 18:39:08 +0800

    Initial Commit

---
+++ OpenCvTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCvTest
+//
+//  Created by 肖家凯 on 15/02/2017.
+//  Copyright © 2017 肖家凯. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
